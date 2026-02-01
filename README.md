f0996c09895e31cef35cea472d2481df6315281a
Chris <thumperfuzzy@gmail.com>
2026-02-01 00:06:50 -0600

    Add outline for ls

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Rewriting some linux shell commands in C.
 
 These are not perfect, nor are they necessairly secure.
 
-This project is mostly a reason for me to program something, fun to program, and helps me learn more about programming in c.
+This project is mostly a reason for me to program something, fun to program, and helps me learn more about programming in c.    
+It also helps me learn more about how linux shell commands work.
---
+++ ls.d/wls.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv){
+	//check for some of the flags ls can use
+	//TODO: implement these
+	//-a: list everything, 
+	//-l list file permissions, number of links, owner, group, size in bytes, last modification date, and name
+	//-R recursively list everything
+	//--help list help information and exit
+
+	//check for folder from user
+	//TODO: implement
+	
+	//if folder does not exist, print error and exit
+	//TODO: implement
+
+	//if no folder location specified, list current directory
+	//TODO: implemet
+
+	return(0);
+}
