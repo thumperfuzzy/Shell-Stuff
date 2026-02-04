84b18adf8083490c595ad3b4468b045743739f85
Chris <thumperfuzzy@gmail.com>
2026-02-04 00:23:09 -0600

    Attempt 1 of getting ls to work. It doesn't

--- ls.d/wls.c
+++ ls.d/wls.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <errno.h>
+
+void printDir(char *name, char *flags){
+	DIR *dirp; 
+	struct dirent *entry;
+
+	dirp = opendir(name);
+	
+	if(!dirp){
+		printf("Cannot open directory %s", name);
+		//_exit(1);
+	}
+	
+	do{
+	errno = 0;
+	entry = readdir(dirp);
+
+
+	if(entry == NULL){
+		if(errno != 0){
+			printf("error");
+		}
+	}
+	
+	//printf("%s, %s", entry->d_type, entry->d_name);
+	//char type[256] = entry->d_name;
+	printf("%s", entry->d_name);
+	}while(entry != NULL);
+
+}
+
 
 int main(int argc, char* argv){
 	//check for some of the flags ls can use
@@ -18,5 +51,9 @@ int main(int argc, char* argv){
 	//if no folder location specified, list current directory
 	//TODO: implemet
 
+	printDir("/", "");
+
 	return(0);
 }
+
+
