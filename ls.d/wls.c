a753c0ccc7c2ee68ab785aaf48c82f13436d8e2d
Chris <thumperfuzzy@gmail.com>
2026-02-07 00:13:24 -0600

    Fixed some issues, created others. Also fixed some documentation.

--- ls.d/wls.c
+++ ls.d/wls.c
@@ -3,18 +3,59 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <string.h>
 
-void printDir(char *name, char *flags){
+
+int printDir(const char *args, int numArgs){
 	DIR *dirp; 
 	struct dirent *entry;
 
-	dirp = opendir(name);
-	
-	if(!dirp){
-		printf("Cannot open directory %s", name);
-		//_exit(1);
+	bool listAll = false;
+	bool recursive = false;
+	bool stats = false;
+
+	char* path;
+	int unusedArgs = numArgs-1;
+	printf("%d", unusedArgs);
+
+	// TODO: Fix errors. What's wrong, idk yet.
+		//			passing flags[i] to strcmp is trying to convert to int‽
+	//check flags
+	for(int i = 1; i <= numArgs; i++){
+		printf("%s\n", &args[i]);
+		if(!strcmp(&args[i], "-a")){
+			listAll = true;
+			unusedArgs--;
+		}
+		if(!strcmp(&args[i], "-R")){
+			recursive = true;
+			unusedArgs--;
+		}
+		if(!strcmp(&args[i], "-l")){
+			stats = true;
+			unusedArgs--;
+		}
+	}
+
+	if(unusedArgs){
+		path = &args[numArgs - 1];
+	}
+	else{
+		path = ".";
 	}
 	
+	printf("Flags: l: %d, a: %d, r: %d\n", stats ? 1 : 0, listAll ? 1 : 0, recursive ? 1 : 0);
+
+	//open directory to read
+	dirp = opendir(path);
+	
+	//if directory does not exist print error message and return 1
+	if(!dirp){
+		printf("Cannot open directory %s", path);
+		return(1);
+	}
+	
+	//read and list all entries within the folder
 	errno = 0;
 	entry = readdir(dirp);
 	while(entry != NULL){
@@ -26,36 +67,39 @@ void printDir(char *name, char *flags){
 		}
 	}
 	
-	//printf("%s, %s", entry->d_type, entry->d_name);
-	//char type[256] = entry->d_name;
-	printf("%s\n", entry->d_name);
+
+	if(strcmp(entry->d_name, "..") > 0){
+		printf("%s\n", entry->d_name);
+	}
 	errno = 0;
 	entry = readdir(dirp);
 	}
 
+	return(0);
+
 }
 
 
-int main(int argc, char* argv){
+int main(int argc, char* argv[]){
+	int res = 0;
+
 	//check for some of the flags ls can use
 	//TODO: implement these
 	//-a: list everything, 
 	//-l list file permissions, number of links, owner, group, size in bytes, last modification date, and name
 	//-R recursively list everything
-	//--help list help information and exit
+	//-h, --help list help information and exit
 
 	//check for folder from user
-	//TODO: implement
-	
-	//if folder does not exist, print error and exit
-	//TODO: implement
-
 	//if no folder location specified, list current directory
-	//TODO: implemet
-
-	printDir(".", "");
-
-	return(0);
+	if(argc > 1){
+		if(!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")){
+			printf("Usage: wls <flags> <path_to_dir>");
+			return(0);
+		}
+	}
+	res = printDir(*argv, argc);
+	return(res);
 }
 
 
